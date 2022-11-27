859d4fc02aad39eefa84c000b3b5a57b388509c9
Swastik Singh <rblswastik@gmail.com>
2022-11-27 11:40:30 +0530

    Update Availability.js

--- components/Availability.js
+++ components/Availability.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 import { View,Text } from 'react-native';
 
 export const Availability= ()=> {
@@ -7,16 +7,28 @@ export const Availability= ()=> {
 //   const [to, setTo] = React.useState("");
 //   const [contact, setContact] = React.useState("");
 //   const [vacant, setVacant] = React.useState(0); 
-const [data,setData]=React.useState([]);
+const [data,setData]=React.useState();
     console.log("show")
     useEffect(() => {
         console.log("in function1");
-        async function fetchData() {
+        async function fetchData() 
+        {
             console.log("in function2");
-            const request = await axios.get("http://10.0.2.2:3000/availability");
-            console.log("data is ",request.data);
-            console.log("in function3")
-            setData(request.data);
+            try 
+            {
+                // const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+                const res = await axios.get("http://192.168.29.220:3000/availability");
+                console.log("in function3");
+                console.log(res.data);
+                setData(res.data);
+            } 
+            catch (error) 
+            {
+               console.log(error);
+            }
+
+            // const request = await axios.get("http://localhost:3000/availability");
+            // console.log("data is ",request.data);
         }
         fetchData();
     }, []);
@@ -24,7 +36,9 @@ const [data,setData]=React.useState([]);
     <>
       <Text>Hello</Text>
       <View>
-        <Text>{JSON.stringify(data)}</Text>
+        <Text>
+            {JSON.stringify(data)}
+        </Text>
       </View>
     </>
   );}
