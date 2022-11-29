79bda9b0c2e982eecf7d4c10eb66091e26fd70b1
Swastik Singh <rblswastik@gmail.com>
2022-11-29 12:32:02 +0530

    Update Availability.js

--- components/Availability.js
+++ components/Availability.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import React, { useEffect,useState } from 'react'
-import { View,Text } from 'react-native';
+import React, { useEffect,useState} from 'react'
+import { View,Text,StyleSheet } from 'react-native';
 
 export const Availability= ()=> {
 //   const[from,setFrom]=React.useState("")
@@ -8,6 +8,7 @@ export const Availability= ()=> {
 //   const [contact, setContact] = React.useState("");
 //   const [vacant, setVacant] = React.useState(0); 
 const [data,setData]=React.useState();
+const [d,setD]=React.useState();
     console.log("show")
     useEffect(() => {
         console.log("in function1");
@@ -17,30 +18,50 @@ const [data,setData]=React.useState();
             try 
             {
                 // const res = await axios.get("https://jsonplaceholder.typicode.com/users");
-                const res = await axios.get("http:192.168.1.10:3000/availability"
+                const res = await axios.get("http:172.16.165.82:3000/availability"
                 );
                 console.log("in function3");
                 console.log(res.data);
-                setData(res.data);
+                 setData(res.data);
+                setD(res.data.from);
             } 
             catch (error) 
             {
-               console.log(error);
+              console.log(error);
             }
-
             // const request = await axios.get("http://localhost:3000/availability");
             // console.log("data is ",request.data);
         }
         fetchData();
     }, []);
-  return (
-    <>
-      <Text>Hello</Text>
-      <View>
-        <Text>
-            {JSON.stringify(data)}
-        </Text>
-      </View>
-    </>
-  );}
-
+    return (
+      <>
+        <View style={styles.body}>
+          <Text >{JSON.stringify(data)}</Text>
+          {/* <Text>{data.journey[0].to}</Text> */}
+          <View>
+            {/* <Text>
+            {data.map((item)=> {
+            return(
+              <View>
+                <Text>From: {item.from}</Text>
+                <Text>To: {item.to}</Text>
+                <Text>Contact: {item.contact}</Text>
+                <Text>Vacant: {item.vacant}</Text>
+              </View>
+            );  
+          })}
+          </Text> */}
+          </View>
+        </View>
+      </>
+    );}
+const styles = StyleSheet.create({
+  body:{
+    display:'flex',
+    flexDirection:'row',
+    height: '100%',
+    paddingLeft: 50,
+  alignItems:"center",
+  }
+});
