1a4f7fbae3d012bfb247932c8e78c13a2117f3b1
Swastik Singh <rblswastik@gmail.com>
2022-11-29 17:14:51 +0530

    Update Availability.js

--- components/Availability.js
+++ components/Availability.js
@@ -7,8 +7,12 @@ export const Availability= ()=> {
 //   const [to, setTo] = React.useState("");
 //   const [contact, setContact] = React.useState("");
 //   const [vacant, setVacant] = React.useState(0); 
-const [data,setData]=React.useState();
+const [data,setData]=React.useState("abcd");
 const [d,setD]=React.useState();
+const [from,setFrom]=React.useState();
+const [to,setTo]=React.useState();
+const [contact,setContact]=React.useState();
+const [vacant,setVacant]=React.useState();
     console.log("show")
     useEffect(() => {
         console.log("in function1");
@@ -18,7 +22,7 @@ const [d,setD]=React.useState();
             try 
             {
                 // const res = await axios.get("https://jsonplaceholder.typicode.com/users");
-                const res = await axios.get("http:172.16.165.82:3000/availability"
+                const res = await axios.get("http:192.168.1.10:3000/availability"
                 );
                 console.log("in function3");
                 console.log(res.data);
@@ -34,25 +38,22 @@ const [d,setD]=React.useState();
         }
         fetchData();
     }, []);
+    const list=()=>{
+      return data.map((item)=>{
+        return(
+          <View>
+            <Text>{item.from}</Text>
+            <Text>{item.to}</Text>
+            <Text>{item.contact}</Text>
+            <Text>{item.vacant}</Text>
+          </View>
+        )
+      })
+    }
     return (
       <>
         <View style={styles.body}>
-          <Text >{JSON.stringify(data)}</Text>
-          {/* <Text>{data.journey[0].to}</Text> */}
-          <View>
-            {/* <Text>
-            {data.map((item)=> {
-            return(
-              <View>
-                <Text>From: {item.from}</Text>
-                <Text>To: {item.to}</Text>
-                <Text>Contact: {item.contact}</Text>
-                <Text>Vacant: {item.vacant}</Text>
-              </View>
-            );  
-          })}
-          </Text> */}
-          </View>
+          {list()}
         </View>
       </>
     );}
@@ -65,3 +66,35 @@ const styles = StyleSheet.create({
   alignItems:"center",
   }
 });
+
+// {
+//   /* <Text>
+//             {JSON.stringify(data[0].contact)}{}
+//           </Text>
+//           <Text>
+//             {JSON.stringify(data[0].from)}{"\n"}
+//           </Text>
+//           <Text>
+//             {JSON.stringify(data[0].to)}{}
+//           </Text>
+//           <Text>
+//             {JSON.stringify(data[0].vacant)}{"\n"}
+//           </Text> */
+// }
+// {
+//   /* <Text>{data.journey[0].to}</Text> */
+// }
+// <View>
+//   {/* <Text>
+//             {data.map((item)=> {
+//             return(
+//               <View>
+//                 <Text>From: {item.from}</Text>
+//                 <Text>To: {item.to}</Text>
+//                 <Text>Contact: {item.contact}</Text>
+//                 <Text>Vacant: {item.vacant}</Text>
+//               </View>
+//             );  
+//           })}
+//           </Text> */}
+// </View>;
